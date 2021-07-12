4d01dfee4f03236f9bf3ca4e87d41a832d6c948c
nlewis84 <github email address>
2021-07-12 16:03:19 -0500

    added some comments re: createHTMLContent

--- apollos-church-api/src/data/ContentItem.js
+++ apollos-church-api/src/data/ContentItem.js
@@ -1,4 +1,6 @@
 import { ContentItem } from '@apollosproject/data-connector-rock';
+// import ApollosConfig from '@apollosproject/config';
+// import sanitizeHtml from 'sanitize-html';
 
 const { schema, resolver } = ContentItem;
 
@@ -48,6 +50,23 @@ class dataSource extends ContentItem.dataSource {
       attributeValues.map(({ entityId }) => entityId)
     ).sort(this.DEFAULT_SORT());
   };
+
+  // createHTMLContent = (content) =>
+  //   sanitizeHtml(content || '', {
+  //     allowedTags: false,
+  //     allowedAttributes: false,
+  //     transformTags: {
+  //       img: (tagName, { src }) => ({
+  //         tagName,
+  //         attribs: {
+  //           // adds Rock URL in the case of local image references in the CMS
+  //           src: src.startsWith('http')
+  //             ? src
+  //             : `${ApollosConfig.ROCK.URL || ''}${src}`,
+  //         },
+  //       }),
+  //     },
+  //   });
 }
 
 export { schema, resolver, dataSource };
