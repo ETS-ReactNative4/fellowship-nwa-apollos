393bad1589ad1415af2e51e48feab18de38ad8ea
nlewis84 <nathan@differential.com>
2021-07-15 13:27:57 -0500

    added campus selector to connect tab

--- apolloschurchapp/src/tabs/connect/Connect.js
+++ apolloschurchapp/src/tabs/connect/Connect.js
@@ -1,10 +1,16 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Query } from '@apollo/client/react/components';
+import { get } from 'lodash';
 
-import { ConnectScreenConnected } from '@apollosproject/ui-connected';
+import {
+  ConnectScreenConnected,
+  GET_USER_PROFILE,
+} from '@apollosproject/ui-connected';
 
 import ActionTable from './ActionTable';
 import ActionBar from './ActionBar';
+import CurrentCampus from './CurrentCampus';
 
 class Connect extends PureComponent {
   static propTypes = {
@@ -15,7 +21,41 @@ class Connect extends PureComponent {
 
   render() {
     return (
-      <ConnectScreenConnected ActionTable={ActionTable} ActionBar={ActionBar} />
+      <ConnectScreenConnected
+        ActionTable={ActionTable}
+        ActionBar={() => (
+          <>
+            <ActionBar />
+            <Query query={GET_USER_PROFILE}>
+              {({ data: campusData, loading: userCampusLoading }) => {
+                const userCampus = get(
+                  campusData,
+                  'currentUser.profile.campus'
+                );
+                return userCampus ? (
+                  <CurrentCampus
+                    cardButtonText={'Campus Details'}
+                    cardTitle={userCampus.name}
+                    coverImage={userCampus.image}
+                    headerActionText={'Change'}
+                    itemId={userCampus.id}
+                    sectionTitle={'Your Campus'}
+                    isLoading={userCampusLoading}
+                  />
+                ) : (
+                  <CurrentCampus
+                    cardButtonText={'Select a Campus'}
+                    cardTitle={'No location'}
+                    headerActionText={'Select a Campus'}
+                    sectionTitle={'Your Campus'}
+                    isLoading={userCampusLoading}
+                  />
+                );
+              }}
+            </Query>
+          </>
+        )}
+      />
     );
   }
 }
