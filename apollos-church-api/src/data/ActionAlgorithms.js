17a23ef3209fedd173aba98875f860189742094e
Nathan Lewis <nathan@differential.com>
2021-09-20 12:40:03 -0500

    added priority sorting algorithm (#20)

--- apollos-church-api/src/data/ActionAlgorithms.js
+++ apollos-church-api/src/data/ActionAlgorithms.js
@@ -3,6 +3,7 @@ import { ActionAlgorithm } from '@apollosproject/data-connector-rock';
 class dataSource extends ActionAlgorithm.dataSource {
   ACTION_ALGORITHMS = {
     ...this.ACTION_ALGORITHMS,
+    PRIORITY_CONTENT_FEED: this.priorityContentFeedAlgorithm.bind(this),
   };
 
   async contentFeedAlgorithm({
@@ -18,6 +19,35 @@ class dataSource extends ActionAlgorithm.dataSource {
         (await ContentItem.byContentChannelId(channel, category))
           .top(limit)
           .skip(skip)
+          .get()
+      )
+    )).flat();
+
+    return items.map((item, i) => ({
+      id: `${item.id}${i}`,
+      title: item.title,
+      subtitle: item.contentChannel?.name,
+      relatedNode: { ...item, __type: ContentItem.resolveType(item) },
+      image: ContentItem.getCoverImage(item),
+      action: 'READ_CONTENT',
+      summary: ContentItem.createSummary(item),
+    }));
+  }
+
+  async priorityContentFeedAlgorithm({
+    category = '',
+    channelIds = [],
+    limit = 20,
+    skip = 0,
+  } = {}) {
+    const { ContentItem } = this.context.dataSources;
+
+    const items = (await Promise.all(
+      channelIds.map(async (channel) =>
+        (await ContentItem.byContentChannelId(channel, category))
+          .top(limit)
+          .skip(skip)
+          .sort([{ field: 'Priority', direction: 'asc' }])
           .get()
       )
     )).flat();
