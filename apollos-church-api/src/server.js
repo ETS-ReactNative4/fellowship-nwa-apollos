a1063093fd71f33c1ed4195eec768e02116d3b31
nlewis84 <nathan@differential.com>
2021-11-22 14:58:15 -0600

    chore: added a comma to clear linter error

--- apollos-church-api/src/server.js
+++ apollos-church-api/src/server.js
@@ -51,7 +51,7 @@ const apolloServer = new ApolloServer({
       shouldReadFromCache: () => !isDev,
       shouldWriteToCache: () => !isDev,
     }),
-    new BugsnagPlugin()
+    new BugsnagPlugin(),
   ],
   formatError: (error) => {
     console.error(get(error, 'extensions.exception.stacktrace', []).join('\n'));
