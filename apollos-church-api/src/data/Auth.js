8d7986cc5debf7978aadfa689128bf993f1cc699
Michael Neeley <micneeley14@gmail.com>
2022-01-12 12:29:54 -0500

    better error logging

--- apollos-church-api/src/data/Auth.js
+++ apollos-church-api/src/data/Auth.js
@@ -6,33 +6,33 @@ import jwt from 'jsonwebtoken';
 const { schema, resolver, contextMiddleware } = Auth;
 
 class dataSource extends Auth.dataSource {
-  // getCurrentPerson = async ({ cookie = null } = { cookie: null }) => {
-  // const { rockCookie, currentPerson } = this.context;
-  // const userCookie = cookie || rockCookie;
-  // if (currentPerson) {
-  // return currentPerson;
-  // }
-  // if (userCookie) {
-  // try {
-  // const request = await this.request('People/GetCurrentPerson').get({
-  // options: {
-  // headers: { cookie: userCookie, 'Authorization-Token': null },
-  // },
-  // });
-  // this.context.currentPerson = request;
-  // return request;
-  // } catch (e) {
-  /// / CUSTOM: took out REDIS lookup stuff
-  /// / const person = await this.lookupUserFromCache({ userCookie });
-  /// / if (!person) {
-  // throw new AuthenticationError(`Invalid Cookie: ${userCookie}`);
-  /// / }
-  /// / TODO: Send over a new cookie to be stored in the `set-cookie` header.
-  /// / return person;
-  // }
-  // }
-  // throw new AuthenticationError('Must be logged in');
-  // };
+  getCurrentPerson = async ({ cookie = null } = { cookie: null }) => {
+    const { rockCookie, currentPerson } = this.context;
+    const userCookie = cookie || rockCookie;
+    if (currentPerson) {
+      return currentPerson;
+    }
+    if (userCookie) {
+      try {
+        const request = await this.request('People/GetCurrentPerson').get({
+          options: {
+            headers: { cookie: userCookie, 'Authorization-Token': null },
+          },
+        });
+        this.context.currentPerson = request;
+        return request;
+      } catch (e) {
+        const person = await this.lookupUserFromCache({ userCookie });
+        if (!person) {
+          throw new AuthenticationError(e);
+        }
+        // TODO: Send over a new cookie to be stored in the `set-cookie` header.
+        return person;
+      }
+    }
+    throw new AuthenticationError('Must be logged in');
+  };
+
   // fetchUserCookie = async (Username, Password) => {
   // try {
   /// / We use `new Response` rather than string/options b/c if conforms more closely with ApolloRESTDataSource
