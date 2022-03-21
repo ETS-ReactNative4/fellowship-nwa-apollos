a519291b6fafbdbb07739dfa4f913cce8f7a3fa9
Michael Neeley <micneeley14@gmail.com>
2022-03-21 10:38:16 -0400

    trying to upgrade them to modern front-end

--- apolloschurchapp/src/client/index.js
+++ apolloschurchapp/src/client/index.js
@@ -1,14 +1,19 @@
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ApolloProvider, ApolloClient, ApolloLink, gql } from '@apollo/client';
+import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries';
+import { sha256 } from 'react-native-sha256';
 import { getVersion, getApplicationName } from 'react-native-device-info';
+import { Platform } from 'react-native';
+import { createUploadLink } from 'apollo-upload-client';
+import ApollosConfig from '@apollosproject/config';
+import { enableFlipperApolloDevtools } from 'react-native-flipper-apollo-devtools';
 
 import { authLink, buildErrorLink } from '@apollosproject/ui-auth';
 import { updatePushId } from '@apollosproject/ui-notifications';
 
 import { NavigationService } from '@apollosproject/ui-kit';
 
-import httpLink from './httpLink';
 import cache, { ensureCacheHydration } from './cache';
 
 const wipeData = () =>
@@ -37,9 +42,36 @@ const onAuthError = async () => {
   NavigationService.resetToAuth();
 };
 
-const errorLink = buildErrorLink(onAuthError);
+// Android's emulator requires localhost network traffic to go through 10.0.2.2
+const uri = ApollosConfig.APP_DATA_URL.replace(
+  'localhost',
+  Platform.OS === 'android' ? '10.0.2.2' : 'localhost'
+);
 
-const link = ApolloLink.from([authLink, errorLink, httpLink]);
+const errorLink = buildErrorLink(onAuthError);
+const apqLink = createPersistedQueryLink({
+  sha256,
+  useGETForHashedQueries: true,
+});
+
+const link = ApolloLink.from([
+  authLink,
+  errorLink,
+  apqLink,
+  createUploadLink({
+    uri,
+    headers: {
+      // Stops Fetch (and the underlying HTTP stack) from caching on our behalf.
+      // Caching should be managed by our server and apollo-client
+      // Fetch handles the `cache-control: private` option in a way that
+      // causes issues when logging in as a different user.
+      'Cache-Control': 'no-cache, no-store',
+      ...(ApollosConfig.CHURCH_HEADER
+        ? { 'x-church': ApollosConfig.CHURCH_HEADER }
+        : {}),
+    },
+  }),
+]);
 
 export const client = new ApolloClient({
   link,
@@ -64,6 +96,7 @@ export const client = new ApolloClient({
     },
   },
 });
+enableFlipperApolloDevtools(client);
 
 wipeData();
 // Ensure that media player still works after logout.
